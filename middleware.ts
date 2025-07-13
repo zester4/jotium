c904127eca8b420eb0bc6f0d5fa8818970d15525
zester4 <champion602@yahoo.com>
2025-07-13 23:30:51 +0000

    tried to fix the payment upgrade issue

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,5 @@ import { authConfig } from "@/app/(auth)/auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: [
-    "/",
-    "/chat/:path*",
-    "/account/:path*",
-    "/pricing/:path*",
-    "/notifications/:path*",
-    "/api/:path*",
-    "/((?!api/pricing/webhook).*)",
-  ],
+  matcher: ["/((?!api/pricing/webhook).*)"],
 };
